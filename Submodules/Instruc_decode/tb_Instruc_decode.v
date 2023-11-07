3440bdb35e67b3873a76e3fb44064141f6698402
aep227 <aep227@vt.edu>
2023-11-06 20:24:10 -0500

    Confirmed functionality of Instruc_decode.v

--- Submodules/Instruc_decode/tb_Instruc_decode.v
+++ Submodules/Instruc_decode/tb_Instruc_decode.v
@@ -1,7 +1,7 @@
 // Using Riviera simulator from EDAplayground
 // Options: -timescale 1ns/1ns -sv2k9
 
-module tb_ALU();
+module tb_Instruc_decode();
 
     reg clk;
     reg [15:0] instruction;
@@ -13,9 +13,11 @@ module tb_ALU();
     wire [2:0] Rs;
     wire [2:0] Rt;
     wire [2:0] Rd;
-    wire [5:0] Imm_Add;
+    wire [2:0] Imm;
+    wire [5:0] Addr;
 
     Instruc_decode DUT(
+      	.clk(clk),
         .instruc(instruction),
         .ALU_op(ALU_op),
         .ALU_src_cntrl(ALU_src_cntrl),
@@ -25,27 +27,95 @@ module tb_ALU();
         .Rs(Rs),
         .Rt(Rt),
         .Rd(Rd),
-        .Imm_Add(Imm_Add)
+        .Imm(Imm),
+        .Addr(Addr)
     );
-
-    inital begin
-        clk = 0;
-    end
-
+  
     always begin
-        clk = ~clk;
         #5
+        clk = ~clk;
     end
 
   initial begin
-    $display("ADD"); // 
-    instruc = 16'h0000;
-    display; 
-
+    clk = 0;
+    $display("ADD $6 $2 $3");
+    instruction = 16'h0993;
+    #10
+    display;
+    $display("SUB $5 $2 $1");
+    instruction = 16'h2951;
+    #10
+    display;
+    $display("MULT $4 $1 $2");
+    instruction = 16'h490A;
+    #10
+    display;
+    $display("DIV $5 $5 $6");
+    instruction = 16'h696E;
+    #10
+    display;
+    $display("AND $5 $4 $1");
+    instruction = 16'h8961;
+    #10
+    display;
+    $display("OR $4 $6 $4");
+    instruction = 16'hA934;
+    #10
+    display;
+    $display("XOR $6 $2 $1");
+    instruction = 16'hC991;
+    #10
+    display;
+    $display("ADDI $5 $5 2");
+    instruction = 16'h1955;
+    #10
+    display;
+    $display("SUBI $1 $4 2");
+    instruction = 16'h3854;
+    #10
+    display;
+    $display("MULTI $6 $5 2");
+    instruction = 16'h5995;
+    #10
+    display;
+    $display("DIVI $6 $3 4");
+    instruction = 16'h79A3;
+    #10
+    display;
+    $display("ANDI $5 $6 2");
+    instruction = 16'h9956;
+    #10
+    display;
+    $display("ORI $6 $5 1");
+    instruction = 16'hB98D;
+    #10
+    display;
+    $display("XORI $4 $1 1");
+    instruction = 16'hD909;
+    #10
+    display;
+    $display("SAVE $2 50");
+    instruction = 16'hE4B2;
+    #10
+    display;
+    $display("LOAD $4 14");
+    instruction = 16'hEB0E;
+    #10
+    display;
   end
   
   task display;
-    #1 $display("result: %8b", result);
+    $display("ALU_op: %3b", ALU_op);
+    $display("ALU_src_cntrl: %1b", ALU_src_cntrl);
+    $display("Reg_write: %1b", Reg_write);
+    $display("Mem_write: %1b", Mem_write);
+    $display("Reg_src_cntrl: %1b", Reg_src_cntrl);
+    $display("Rs: %3b", Rs);
+    $display("Rt: %3b", Rt);
+    $display("Rd: %3b", Rd);
+    $display("Imm: %3b", Imm);
+    $display("Addr: %6b", Addr);
+    $display(" ");
   endtask
 
 endmodule
